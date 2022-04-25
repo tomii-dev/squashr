e65cb36df1b14b84f9d5ca7099539d14335ac285
tomii <hellotommybowles@gmail.com>
2022-04-25 11:43:38 +0100

    project page view model

---
+++ squashr/ViewModels/ProjectViewModel.cs
@@ -0,0 +1,23 @@
+﻿using squashr.Models;
+
+namespace squashr.ViewModels
+{
+    public class ProjectViewModel : ViewModelBase
+    {
+        private Project _project;
+        public string Title { get; set; }
+        public Project Project
+        {
+            get { return _project; }
+            set { 
+                _project = value;
+                Update();
+            }
+        }
+
+        private void Update()
+        {
+            Title = _project.Name;
+        }
+    }
+}
