3f788915707886d7dd360c79d4fcd27ad083c348
tomii <hellotommybowles@gmail.com>
2022-04-25 11:42:34 +0100

    create project page

---
+++ squashr/Views/ProjectView.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Controls;
+
+namespace squashr.Views
+{
+    public partial class ProjectView : UserControl
+    {
+        public ProjectView()
+        {
+            InitializeComponent();
+        }
+    }
+}
